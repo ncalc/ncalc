e1d07115914d9d3fb0b6771f1cd3b0fb94621973
Radim Holek <radimholek@gmail.com>
2022-05-30 13:31:52 +0200

    add test for changing culture

--- test/NCalc.Tests/Fixtures.cs
+++ test/NCalc.Tests/Fixtures.cs
@@ -762,5 +762,72 @@ namespace NCalc.Tests
                 Thread.CurrentThread.CurrentCulture = originalCulture;
             }
         }
+
+        [TestMethod]
+        public void ShouldCorrectlyParseCustomCultureParameter()
+        {
+            var cultureDot = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+            cultureDot.NumberFormat.NumberGroupSeparator = " ";
+            var cultureComma = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+            cultureComma.NumberFormat.CurrencyDecimalSeparator = ",";
+            cultureComma.NumberFormat.NumberGroupSeparator = " ";
+
+            //use 1*[A] to avoid evaluating expression parameters as string - force numeric conversion
+            ExecuteTest("1*[A]-[B]", 1.5m);
+            ExecuteTest("1*[A]+[B]", 2.5m);
+            ExecuteTest("1*[A]/[B]", 4m);
+            ExecuteTest("1*[A]*[B]", 1m);
+            ExecuteTest("1*[A]>[B]", true);
+            ExecuteTest("1*[A]<[B]", false);
+
+
+            void ExecuteTest(string formula, object expectedValue)
+            {
+                //Correctly evaluate with decimal dot culture and parameter with dot
+                Assert.AreEqual(expectedValue, new Expression(formula, cultureDot)
+                {
+                    Parameters = new Dictionary<string, object>
+                        {
+                            {"A","2.0"},
+                            {"B","0.5"}
+
+                        }
+                }.Evaluate());
+
+                //Correctly evaluate with decimal comma and parameter with comma
+                Assert.AreEqual(expectedValue, new Expression(formula, cultureComma)
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        {"A","2.0"},
+                        {"B","0.5"}
+
+                    }
+                }.Evaluate());
+
+                //combining decimal dot and comma fails
+                Assert.ThrowsException<FormatException>(() => new Expression(formula, cultureComma)
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        {"A","2,0"},
+                        {"B","0.5"}
+
+                    }
+                }.Evaluate());
+
+                //combining decimal dot and comma fails
+                Assert.ThrowsException<FormatException>(() => new Expression(formula, cultureDot)
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        {"A","2,0"},
+                        {"B","0.5"}
+
+                    }
+                }.Evaluate());
+
+            }
+        }
     }
 }
