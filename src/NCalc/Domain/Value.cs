5694837ac2b1beaf49dcac01d4474431886cdb33
Thomas Hambach <505642+ThomasHambach@users.noreply.github.com>
2022-11-29 19:21:08 +0800

    Add parameterless constructor to ValueExpression (#61)

--- src/NCalc/Domain/Value.cs
+++ src/NCalc/Domain/Value.cs
@@ -4,6 +4,8 @@ namespace NCalc.Domain
 {
     public class ValueExpression : LogicalExpression
     {
+        public ValueExpression() {}
+        
         public ValueExpression(object value, ValueType type)
         {
             Value = value;
