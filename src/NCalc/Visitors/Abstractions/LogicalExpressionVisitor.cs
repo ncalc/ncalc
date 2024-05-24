b8864df47e344113e43ec269a0bf8cab9fe92eaa
gumbarros <gustavomauriciodebarros@gmail.com>
2024-05-24 12:02:15 -0300

    Prevent breaking change.

---
+++ src/NCalc/Visitors/Abstractions/LogicalExpressionVisitor.cs
@@ -0,0 +1,15 @@
+﻿using NCalc.Domain;
+
+namespace NCalc.Visitors;
+
+[Obsolete("Please use ILogicalExpressionVisitor.")]
+public abstract class LogicalExpressionVisitor : ILogicalExpressionVisitor
+{
+    public abstract void Visit(LogicalExpression expression);
+    public abstract void Visit(TernaryExpression expression);
+    public abstract void Visit(BinaryExpression expression);
+    public abstract void Visit(UnaryExpression expression);
+    public abstract void Visit(ValueExpression expression);
+    public abstract void Visit(Function function);
+    public abstract void Visit(Identifier function);
+}
