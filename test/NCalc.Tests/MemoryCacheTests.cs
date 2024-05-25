40216733377fdd8b97a3ecc40c7958859f10730c
gumbarros <gustavomauriciodebarros@gmail.com>
2024-05-25 14:50:41 -0300

    Remove `Logical_Expression_From_Cache_Should_Be_The_Same`

--- test/NCalc.Tests/MemoryCacheTests.cs
+++ test/NCalc.Tests/MemoryCacheTests.cs
@@ -7,20 +7,6 @@ namespace NCalc.Tests;
 public class MemoryCacheTests(FactoriesWithMemoryCacheFixture fixture) : IClassFixture<FactoriesWithMemoryCacheFixture>
 {
     private readonly IExpressionFactory _expressionFactory = fixture.ExpressionFactory;
-
-    [Fact]
-    public void Logical_Expression_From_Cache_Should_Be_The_Same()
-    {
-        var expression = _expressionFactory.Create("42 == True");
-        
-        Assert.Equal(false,expression.Evaluate());
-        
-        var anotherExpression = _expressionFactory.Create("42 == True");
-        
-        Assert.Equal(false,anotherExpression.Evaluate());
-        
-        Assert.Equal(expression.LogicalExpression,anotherExpression.LogicalExpression);
-    }
     
     [Fact]
     public void Logical_Expression_Without_Cache_Should_Not_Be_The_Same()
