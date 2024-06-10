28d0ad7ece39cdcecab7813eb5403f62c1b51552
gumbarros <gustavomauriciodebarros@gmail.com>
2024-06-09 22:11:56 -0300

    Allow whitespace at end of expression

---
+++ test/NCalc.Tests/ParserTests.cs
@@ -0,0 +1,17 @@
+﻿namespace NCalc.Tests;
+
+[Trait("Category","Parser")]
+public class ParserTests
+{
+    [Theory]
+    [InlineData("11+33 ", 44)]
+    [InlineData(" 11+33", 44)]
+    [InlineData(" 11+33 ", 44)]
+    public void ShouldIgnoreWhitespacesIssue222(string formula, object expectedValue)
+    {
+        var expr = new Expression(formula, CultureInfo.InvariantCulture);
+        var res = expr.Evaluate();
+
+        Assert.Equal(expectedValue, res);
+    }
+}
