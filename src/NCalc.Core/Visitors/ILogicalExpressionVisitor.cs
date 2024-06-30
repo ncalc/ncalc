96ea0a4258d354b04bf8b8927be45e7092aee6c0
gumbarros <gustavomauriciodebarros@gmail.com>
2024-06-30 19:24:01 -0300

    Added xmldoc

--- src/NCalc.Core/Visitors/ILogicalExpressionVisitor.cs
+++ src/NCalc.Core/Visitors/ILogicalExpressionVisitor.cs
@@ -2,6 +2,10 @@ using NCalc.Domain;
 
 namespace NCalc.Visitors;
 
+/// <summary>
+/// Defines methods to visit different types of logical expressions in an abstract syntax tree (AST).
+/// </summary>
+/// <typeparam name="T">The type of result returned from each visit method.</typeparam>
 public interface ILogicalExpressionVisitor<out T>
 {
     T Visit(TernaryExpression expression);
