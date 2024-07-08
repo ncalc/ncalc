ef0fc4218ee707eebb64139677319dac118cf9ff
gumbarros <gustavomauriciodebarros@gmail.com>
2024-07-08 14:14:05 -0300

    Add `sealed` modifier to `LogicalExpressionParserContext`

--- src/NCalc.Core/Parser/LogicalExpressionParserContext.cs
+++ src/NCalc.Core/Parser/LogicalExpressionParserContext.cs
@@ -3,7 +3,7 @@ using Parlot.Fluent;
 
 namespace NCalc.Parser;
 
-public class LogicalExpressionParserContext(string text, ExpressionOptions options)
+public sealed class LogicalExpressionParserContext(string text, ExpressionOptions options)
     : ParseContext(new Scanner(text))
 {
     public ExpressionOptions Options { get; } = options;
