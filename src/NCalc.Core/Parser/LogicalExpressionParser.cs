acd780391fa359d654008b5a2f8cfb9b0e6b5632
gumbarros <gustavomauriciodebarros@gmail.com>
2024-09-25 20:19:33 -0300

    Fix regression for bitwise operators

--- src/NCalc.Core/Parser/LogicalExpressionParser.cs
+++ src/NCalc.Core/Parser/LogicalExpressionParser.cs
@@ -399,31 +399,13 @@ public static class LogicalExpressionParser
                     notLike.Then(BinaryExpressionType.NotLike)
                 )
                 .And(shift)))
-            .Then(static x =>
-            {
-                var result = x.Item1;
-                foreach (var op in x.Item2)
-                {
-                    result = new BinaryExpression(op.Item1, result, op.Item2);
-                }
-
-                return result;
-            });
+            .Then(GetBinaryExpression);
 
         var equality = relational.And(ZeroOrMany(OneOf(
                     equal.Then(BinaryExpressionType.Equal),
                     notEqual.Then(BinaryExpressionType.NotEqual))
                 .And(relational)))
-            .Then(static x =>
-            {
-                var result = x.Item1;
-                foreach (var op in x.Item2)
-                {
-                    result = new BinaryExpression(op.Item1, result, op.Item2);
-                }
-
-                return result;
-            });
+            .Then(GetBinaryExpression);
 
         var andTypeParser = and.Then(BinaryExpressionType.And)
             .Or(bitwiseAnd.Then(BinaryExpressionType.BitwiseAnd));
@@ -435,38 +417,14 @@ public static class LogicalExpressionParser
 
         // "and" has higher precedence than "or"
         var andParser = equality.And(ZeroOrMany(andTypeParser.And(equality)))
-            .Then(static x =>
-            {
-                var result = x.Item1;
-                foreach (var op in x.Item2)
-                {
-                    result = new BinaryExpression(BinaryExpressionType.And, result, op.Item2);
-                }
-                return result;
-            });
+            .Then(GetBinaryExpression);
 
         var orParser = andParser.And(ZeroOrMany(orTypeParser.And(andParser)))
-            .Then(static x =>
-            {
-                var result = x.Item1;
-                foreach (var op in x.Item2)
-                {
-                    result = new BinaryExpression(BinaryExpressionType.Or, result, op.Item2);
-                }
-                return result;
-            });
+            .Then(GetBinaryExpression);
 
         // logical => equality ( ( "and" | "or" | "xor" ) equality )* ;
         var logical = orParser.And(ZeroOrMany(xorTypeParser.And(orParser)))
-            .Then(static x =>
-            {
-                var result = x.Item1;
-                foreach (var op in x.Item2)
-                {
-                    result = new BinaryExpression(BinaryExpressionType.BitwiseXOr, result, op.Item2);
-                }
-                return result;
-            });
+            .Then(GetBinaryExpression);
 
         // ternary => logical("?" logical ":" logical) ?
         var ternary = logical.And(ZeroOrOne(questionMark.SkipAnd(logical).AndSkip(colon).And(logical)))
@@ -490,6 +448,18 @@ public static class LogicalExpressionParser
         AppContext.TryGetSwitch("NCalc.EnableParlotParserCompilation", out var enableParserCompilation);
 
         Parser = enableParserCompilation ? expressionParser.Compile() : expressionParser;
+    }
+
+    private static LogicalExpression GetBinaryExpression((LogicalExpression, IReadOnlyList<(BinaryExpressionType, LogicalExpression)>) x)
+    {
+        var result = x.Item1;
+
+        foreach (var op in x.Item2)
+        {
+            result = new BinaryExpression(op.Item1, result, op.Item2);
+        }
+
+        return result;
     }
 
     public static LogicalExpression Parse(LogicalExpressionParserContext context)
