3bd95f2a2d5f1471f05494e0e1a5147f18e24447
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2024-11-15 16:40:30 -0300

    Remove obsolete `Expressions` property from `Function` class (#362)

--- src/NCalc.Core/Domain/Function.cs
+++ src/NCalc.Core/Domain/Function.cs
@@ -8,13 +8,6 @@ public sealed class Function(Identifier identifier, LogicalExpressionList parame
 
     public LogicalExpressionList Parameters { get; set; } = parameters;
 
-    [Obsolete("Please use Parameters property.")]
-    public LogicalExpression[] Expressions
-    {
-        get => Parameters.ToArray();
-        set => Parameters = new LogicalExpressionList(value);
-    }
-
     public override T Accept<T>(ILogicalExpressionVisitor<T> visitor)
     {
         return visitor.Visit(this);
