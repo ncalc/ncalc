1a4fc12b0736b2a716c453b42860b95e28696c41
ABykiev <bykiev.andrei@mail.ru>
2025-05-16 18:54:37 +0300

    Add parser compilation benchmarks
    Add parser compilation benchmarks

---
+++ test/NCalc.Benchmarks/ParserCompilationBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using NCalc.Domain;
+using NCalc.Parser;
+
+namespace NCalc.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class WithoutCompilationBenchmark
+    {
+        private const string Expression = "(a + b * 2) > 10 and value in (20, 30, 40)";
+
+        [GlobalSetup]
+        public void Setup() => AppContext.SetSwitch("NCalc.EnableParlotParserCompilation", false);
+
+        [Benchmark]
+        public LogicalExpression Parse() => LogicalExpressionParser.Parse(new LogicalExpressionParserContext(Expression, ExpressionOptions.None));
+    }
+
+    [MemoryDiagnoser]
+    public class WithCompilationBenchmark
+    {
+        private const string Expression = "(a + b * 2) > 10 and value in (20, 30, 40)";
+
+        [GlobalSetup]
+        public void Setup() => AppContext.SetSwitch("NCalc.EnableParlotParserCompilation", true);
+
+        [Benchmark]
+        public LogicalExpression Parse() => LogicalExpressionParser.Parse(new LogicalExpressionParserContext(Expression, ExpressionOptions.None));
+    }
+}
