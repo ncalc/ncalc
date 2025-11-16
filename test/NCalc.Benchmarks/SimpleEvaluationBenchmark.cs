f0d91431c3d91768e281904cb780ec84388a6003
ABykiev <bykiev.andrei@mail.ru>
2025-11-16 23:52:41 +0300

    Update to .net 10

--- test/NCalc.Benchmarks/SimpleEvaluationBenchmark.cs
+++ test/NCalc.Benchmarks/SimpleEvaluationBenchmark.cs
@@ -1,10 +1,13 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Order;
 using NCalc.Factories;
 using NCalc.Handlers;
 
 namespace NCalc.Benchmarks;
 
+[SimpleJob(RuntimeMoniker.Net80)]
+[SimpleJob(RuntimeMoniker.Net10_0)]
 [RankColumn]
 [CategoriesColumn]
 [MemoryDiagnoser]
