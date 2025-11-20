208f7a3dc1bc25d75128c2af2bde46af1e5b1a17
ABykiev <bykiev.andrei@mail.ru>
2025-11-20 14:11:21 +0300

    Update Program.cs

--- test/NCalc.Benchmarks/Program.cs
+++ test/NCalc.Benchmarks/Program.cs
@@ -5,3 +5,5 @@ BenchmarkRunner.Run<LogicalExpressionFactoryBenchmark>(null, args);
 BenchmarkRunner.Run<SimpleEvaluationBenchmark>(null, args);
 BenchmarkRunner.Run<EvaluateVsLambdaBenchmark>(null, args);
 BenchmarkRunner.Run<NCalcVsDataTableBenchmark>(null, args);
+BenchmarkRunner.Run<WithoutCompilationBenchmark>(null, args);
+BenchmarkRunner.Run<WithCompilationBenchmark>(null, args);
