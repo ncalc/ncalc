d7c1eca677b2d682a6196948d248f279a6d0862d
Andrey Bykiev <bykiev.andrei@mail.ru>
2025-12-18 21:10:32 +0300

    Disable parser loop detection (#527)
    As the parser is fully under our control infinite circle on parser level is impossible. Disabling this feature will improve the overall performance

--- src/NCalc.Core/Parser/LogicalExpressionParserContext.cs
+++ src/NCalc.Core/Parser/LogicalExpressionParserContext.cs
@@ -4,12 +4,13 @@ using Parlot.Fluent;
 namespace NCalc.Parser;
 
 public sealed class LogicalExpressionParserContext(string text, ExpressionOptions options, CancellationToken ct = default)
-    : ParseContext(new Scanner(text), ct)
+    : ParseContext(new Scanner(text), false, true, ct)
 {
     /// <summary>
     /// Parser options containing culture info and argument separator settings.
     /// </summary>
     public LogicalExpressionParserOptions ParserOptions { get; init; } = LogicalExpressionParserOptions.Default;
+
     public ExpressionOptions Options { get; } = options;
 
     public CultureInfo CultureInfo { get; } = CultureInfo.CurrentCulture;
