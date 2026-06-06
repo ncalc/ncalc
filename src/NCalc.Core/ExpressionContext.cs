0eb70bb6535983ee334760a231cfca9e22ff0c2a
gumbarros <gustavomauriciodebarros@gmail.com>
2026-06-06 16:23:54 -0300

    Add new constructors overloads to ExpressionContext.cs. This closes #539

--- src/NCalc.Core/ExpressionContext.cs
+++ src/NCalc.Core/ExpressionContext.cs
@@ -5,30 +5,52 @@ namespace NCalc;
 
 public record ExpressionContext
 {
-    public IDictionary<string, ExpressionParameter> DynamicParameters { get; set; } =
-        new Dictionary<string, ExpressionParameter>();
-
-    public IDictionary<string, ExpressionFunction> Functions { get; set; } =
-        new Dictionary<string, ExpressionFunction>();
-
-    public IDictionary<string, AsyncExpressionFunction> AsyncFunctions { get; set; } = new Dictionary<string, AsyncExpressionFunction>();
+    public IDictionary<string, object?> StaticParameters { get; set; }
+    public IDictionary<string, ExpressionParameter> DynamicParameters { get; set; }
+    public IDictionary<string, ExpressionFunction> Functions { get; set; }
+    public IDictionary<string, AsyncExpressionFunction> AsyncFunctions { get; set; }
 
     public EvaluateParameterHandler? EvaluateParameterHandler { get; set; }
     public EvaluateFunctionHandler? EvaluateFunctionHandler { get; set; }
     public EvaluateAsyncFunctionHandler? EvaluateAsyncFunctionHandler { get; set; }
 
-    public ExpressionOptions Options { get; set; } = ExpressionOptions.None;
-    public CultureInfo CultureInfo { get; set; } = CultureInfo.CurrentCulture;
-    public IDictionary<string, object?> StaticParameters { get; set; } = new Dictionary<string, object?>();
+    public ExpressionOptions Options { get; set; }
+    public CultureInfo CultureInfo { get; set; }
 
-    public ExpressionContext()
+    public ExpressionContext() : this(ExpressionOptions.None, CultureInfo.CurrentCulture)
     {
     }
 
-    public ExpressionContext(ExpressionOptions options, CultureInfo? cultureInfo)
+    public ExpressionContext(
+        IDictionary<string, object?>? staticParameters,
+        IDictionary<string, ExpressionParameter>? dynamicParameters = null,
+        IDictionary<string, ExpressionFunction>? functions = null,
+        IDictionary<string, AsyncExpressionFunction>? asyncFunctions = null)
+        : this(
+            ExpressionOptions.None,
+            CultureInfo.CurrentCulture,
+            staticParameters,
+            dynamicParameters,
+            functions,
+            asyncFunctions)
+    {
+    }
+
+    public ExpressionContext(
+        ExpressionOptions options,
+        CultureInfo? cultureInfo,
+        IDictionary<string, object?>? staticParameters = null,
+        IDictionary<string, ExpressionParameter>? dynamicParameters = null,
+        IDictionary<string, ExpressionFunction>? functions = null,
+        IDictionary<string, AsyncExpressionFunction>? asyncFunctions = null)
     {
         Options = options;
         CultureInfo = cultureInfo ?? CultureInfo.CurrentCulture;
+
+        StaticParameters = staticParameters ?? new Dictionary<string, object?>();
+        DynamicParameters = dynamicParameters ?? new Dictionary<string, ExpressionParameter>();
+        Functions = functions ?? new Dictionary<string, ExpressionFunction>();
+        AsyncFunctions = asyncFunctions ?? new Dictionary<string, AsyncExpressionFunction>();
     }
 
     public static implicit operator ExpressionContext(ExpressionOptions options) => new()
