eb780eb33c850e963e8c8fc6585311d6edac261b
Andrey Bykiev <bykiev.andrei@mail.ru>
2026-09-08 22:30:35 +0300

    Fix skipped CI steps (#623)
    This PR fixes skipped steps on CI. Added SkipInNativeAotAttribute for TUnit. For System.Text.Json source generators should be used...

---
+++ test/NCalc.Tests/Attributes/SkipInNativeAotAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace NCalc.Tests.Attributes
+{
+    public class SkipInNativeAotAttribute : SkipAttribute
+    {
+        public SkipInNativeAotAttribute() : base("Reflection JSON deserialization is not supported under NativeAOT.")
+        {
+        }
+
+        public override Task<bool> ShouldSkip(TestRegisteredContext context)
+        {
+            bool isAot = !System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeCompiled;
+            return Task.FromResult(isAot);
+        }
+    }
+}
